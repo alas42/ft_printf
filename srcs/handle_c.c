41c0a17df12789bf5b75a0e2577969e351314680
unknown <alexandre.vogt7@etu.univ-lorraine.fr>
2020-06-23 15:13:22 +0200

    handle_c

--- srcs/handle_c.c
+++ srcs/handle_c.c
@@ -14,5 +14,23 @@
 
 t_tab	*handle_c(t_tab *tab)
 {
+	char		*s;
+	char		c;
+	long int	i;
+
+	i = -1;
+	c = (char)va_arg(tab->ap, int);
+	s = (tab->argument->field_width > 1) ? 
+		ft_strnew(tab->argument->field_width) : ft_strnew(1);
+	if (!tab->argument->flags[0] && tab->argument->field_width > 1)
+		while (++i < tab->argument->field_width - 1)
+			s[i] = ' ';
+	i = (i == -1) ? 0 : i;
+	s[i] = c;
+	if (tab->argument->flags[0] && tab->argument->field_width > 1)
+		while (i++ < tab->argument->field_width - 1)
+			s[i] = ' ';
+	ft_putstr(s);
+	free(s);
 	return (tab);
 }
