fb31b19f7b154779742d7fb4bb509b0c0508e05c
Alexandre VOGT <avogt@e2r9p3.42.fr>
2020-06-23 15:39:49 +0200

    wtf edition 2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: avogt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 16:36:52 by avogt             #+#    #+#             */
-/*   Updated: 2020/06/23 15:16:29 by avogt            ###   ########.fr       */
+/*   Updated: 2020/06/23 15:39:22 by avogt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(void)
 {
-	char	c;
-	unsigned int test;
-	int champ;
-	int precision;
+	char			*s;
+	char			c;
+	unsigned int	test;
+	int 			champ;
+	int 			precision;
 
+	s = "Hello World, test!";
 	champ = 10;
 	precision = 5;
 	test = 30;
@@ -66,5 +68,16 @@ int	main(void)
 	printf("%-5c 0\n", c);
 	ft_printf("%5c 45\n", c);
 	printf("%5c 45\n", c);
+
+	ft_printf("%s\n", s);
+	printf("%s\n", s);
+	ft_printf("%5s\n", s);
+	printf("%5s\n", s);
+	ft_printf("%-5s\n", s);
+	printf("%-5s\n", s);
+	ft_printf("%-s 0\n", s);
+	printf("%-s 0\n", s);
+	ft_printf("%.5s 45\n", s);
+	printf("%.5s 45\n", s);
 	return (0);
 }
