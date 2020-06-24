90b4ce39fd8ef80136f0494fa92481078c2ded26
unknown <alexandre.vogt7@etu.univ-lorraine.fr>
2020-06-24 20:59:24 +0200

    p without norminette and gcc

--- srcs/handle_p.c
+++ srcs/handle_p.c
@@ -12,7 +12,99 @@
 
 #include "ft_printf.h"
 
+static int	fill_result2(char *string, t_tab *tab, char *arg, long int pos)
+{
+	long int i;
+	long int j;
+	long int c;
+	long int len_arg;
+
+	len_arg = (long int)ft_strlen(arg);
+	i = pos;
+	j = (i > 0 && tab->argument->precision > len_arg) ? i - (tab->argument->precision - len_arg) : 0;
+	c = -1;
+	while (++c < tab->argument->precision - len_arg)
+	{
+		s[j + c] = '0';
+		i = (j == 0) ? i + 1 : i;
+	}
+	j = 0;
+	while (j < len_arg)
+		s[i++] = result[j++];
+	if (tab->argument->flags[0])
+		if (tab->argument->field_width > len_arg)
+			while (i < tab->argument->field_width)
+				s[i++] = ' ';
+	return (1);
+}
+
+static int	fill_result(char *string, t_tab *tab, char *arg, long int len_arg)
+{
+	long int i;
+	long int c;
+	long int j;
+
+	j = 0;
+	c = 0;
+	i = 0;
+	if (!tab->argument->flags[0])
+	{
+		c = (tab->argument->precision < len_arg - 2) ? tab->argument->field_width - (len_arg - 2)
+			: tab->argument->field_width - (tab->argument->precision + 2);
+		if (c > 0)
+			while (i < c)
+			{
+				if (tab->argument->flags[1] && tab->argument->precision == -1)
+				{
+					if (i == 0)
+					{
+						string[i++] = '0';
+						string[i] = 'x';
+						j += 2;
+					}
+					else
+						string[i] = '0';
+				}
+				else
+					string[i] = ' ';
+				i++;
+			}
+	}
+	if (fill_result2(string, tab, arg, i))
+		return (1);
+	else
+		return (-1);
+}
+
+char		*get_string_p(char *arg, t_tab *tab, long int len_arg)
+{
+	char	*string;
+
+	string = NULL;
+	if (tab->argument->field_width >= tab->argument->precision + 2 && tab->argument->field_width > len_arg)
+		string = ft_strnew(tab->argument->field_width);
+	else if (tab->argument->field_width < tab->argument->precision + 2 && tab->argument->precision + 2 > len_arg)
+		string = ft_strnew(tab->argument->precision + 2);
+	else
+		string = ft_strnew(len_arg);
+	if (fill_result(string, tab, arg, len_arg))
+		return (string);
+	return (NULL);
+}
+
 t_tab	*handle_p(t_tab *tab)
 {
+	char	*arg_to_print;
+	char	*s;
+	size_t	len_to_print;
+
+	len_to_print = 0;
+	arg_to_print = ft_convert_base_l((unsigned long int)va_arg(ap, void *), 16);
+	printf("arg_to_print : --%s-- len_arg : --%ld-- field_width : --%ld-- precision : --%ld--\n", arg_to_print, (long int)ft_strlen(arg_to_print), tab->argument->field_width, tab->argument->precision);
+	s = get_string_p(arg_to_print, tab, (long int)ft_strlen(arg_to_print));
+	len_to_print = ft_strlen(s);
+	ft_putstr(s);
+	free(s);
+	free(arg_to_print);
 	return (tab);
 }
