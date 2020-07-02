21b34b92c888587b167474a6c7cc4580494c37c3
unknown <alexandre.vogt7@etu.univ-lorraine.fr>
2020-07-02 14:34:49 +0200

    d copy to i

--- srcs/handle_i.c
+++ srcs/handle_i.c
@@ -19,7 +19,7 @@ static long int	is_min(char *string, t_tab *tab, char *arg, long int pos)
 	long int len_arg;
 
 	len_arg = ft_strlen(arg);
-	c= 0;
+	c = 0;
 	i = 0;
 	if (arg[0] == '-')
 	{
@@ -66,7 +66,7 @@ static int		f_str_2(char *string, t_tab *tab, char *arg, long int len_arg)
 	return (1);
 }
 
-static int	f_str_1(char *string, t_tab *tab, char *arg, long int len_arg)
+static int		f_str_1(char *string, t_tab *tab, char *arg, long int len_arg)
 {
 	long int	c;
 	long int	i;
@@ -100,15 +100,15 @@ static int	f_str_1(char *string, t_tab *tab, char *arg, long int len_arg)
 		return (f_str_2(string, tab, arg, len_arg));
 }
 
-static char	*get_string_i(char *arg_to_print, t_tab *tab, int arg)
+static char		*get_string_d(char *arg_to_print, t_tab *tab, int arg)
 {
 	char		*string;
 	long int	len_arg;
 	char		signe;
 
 	signe = (arg < 0) ? '-' : '+';
-	len_arg = (long int) ft_strlen(arg_to_print);
-	len_arg = (signe == '-') ? len_arg -1 : len_arg;
+	len_arg = (long int)ft_strlen(arg_to_print);
+	len_arg = (signe == '-') ? len_arg - 1 : len_arg;
 	string = NULL;
 	if (tab->argument->field_width >= tab->argument->precision
 		&& tab->argument->field_width > len_arg)
@@ -121,14 +121,14 @@ static char	*get_string_i(char *arg_to_print, t_tab *tab, int arg)
 		string = ft_strnew(len_arg + 1);
 	else
 		string = ft_strnew(len_arg);
-	if (f_str_1(string, tab, arg_to_print, (long int) ft_strlen(arg_to_print)))
+	if (f_str_1(string, tab, arg_to_print, (long int)ft_strlen(arg_to_print)))
 		return (string);
 	return (NULL);
 }
 
-t_tab		*handle_i(t_tab *tab)
+t_tab			*handle_i(t_tab *tab)
 {
-	int 		arg;
+	int			arg;
 	char		*arg_to_print;
 	char		*string;
 	size_t		len_to_print;
@@ -136,9 +136,9 @@ t_tab		*handle_i(t_tab *tab)
 	len_to_print = 0;
 	arg = va_arg(tab->ap, int);
 	arg_to_print = ft_itoa(arg);
-	string = get_string_i(arg_to_print, tab, arg);
+	string = get_string_d(arg_to_print, tab, arg);
 	if (string == NULL)
-		exit (-1);
+		exit(-1);
 	len_to_print = ft_strlen(string);
 	ft_putstr(string);
 	ft_strdel(&string);
