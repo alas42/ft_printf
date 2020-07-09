c30362a3f671519ea064eaa01a164992dc09aa09
unknown <alexandre.vogt7@etu.univ-lorraine.fr>
2020-07-09 09:55:42 +0200

    tetet

--- srcs/handle_c.c
+++ srcs/handle_c.c
@@ -18,13 +18,13 @@ t_tab	*handle_c(t_tab *tab)
 
 	i = -1;
 	c = (char)va_arg(tab->ap, int);
-	if (tab->convert[1] == '0' && tab->convert[0] != '-')
+	if (tab->arg->flags[1] == '0' && tab->arg->flags[0] != '-')
 		display_gap(tab, '0', tab->arg->width - 1, 1);
-	else if (tab->convert[0] != '-')
+	else if (tab->arg->flags[0] != '-')
 		display_gap(tab, ' ', tab->arg->width - 1, 1);
 	tab->len += 1;
 	write(1, &c, 1);
-	if (tab->convert[0] == '-')
+	if (tab->arg->flags[0] == '-')
 		display_char(tab, ' ', tab->arg->width - 1, 1);
 	return (tab);
 }
