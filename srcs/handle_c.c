36faba8a92d2839ec219298edd61000b4c6c36f8
unknown <alexandre.vogt7@etu.univ-lorraine.fr>
2020-07-09 09:56:37 +0200

    tetet

--- srcs/handle_c.c
+++ srcs/handle_c.c
@@ -16,12 +16,11 @@ t_tab	*handle_c(t_tab *tab)
 {
 	char		c;
 
-	i = -1;
 	c = (char)va_arg(tab->ap, int);
 	if (tab->arg->flags[1] == '0' && tab->arg->flags[0] != '-')
-		display_gap(tab, '0', tab->arg->width - 1, 1);
+		display_char(tab, '0', tab->arg->width - 1, 1);
 	else if (tab->arg->flags[0] != '-')
-		display_gap(tab, ' ', tab->arg->width - 1, 1);
+		display_char(tab, ' ', tab->arg->width - 1, 1);
 	tab->len += 1;
 	write(1, &c, 1);
 	if (tab->arg->flags[0] == '-')
